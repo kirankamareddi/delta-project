ba49ed6cae1e206b396e3b8e3ad70c0eccfcf4bc
Delto Student <kirankamareddi@gmail.com>
2026-09-13 09:37:12 -0700

    Add Project Files

---
+++ models/listing.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Review = require("./review.js");
+
+const listingSchema = new Schema({
+     title: {
+        type: String,
+        required: true,
+      },
+     description: String,
+      image: {
+        filename: {
+            type: String,
+            default: "listingimage",
+        },
+        url: {
+            type: String,
+            default: "https://images.unsplash.com/photo-1564013799919-ab600027ffc6",
+            set: (v) => v === "" ? "https://images.unsplash.com/photo-1564013799919-ab600027ffc6" : v,
+        },
+    },
+     price: Number,
+     location: String,
+     country: String,
+     reviews: [
+          {
+             type: mongoose.Schema.Types.ObjectId,
+             ref: "Review",
+          },
+    ],
+	owner: {
+		type: Schema.Types.ObjectId,
+		ref: "User",
+	}
+});
+
+
+listingSchema.post("findOneAndDelete",async (listing) => {
+	if(listing){
+		await Review.deleteMany({_id: {$in: listing.reviews}});
+	}	
+	 
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
